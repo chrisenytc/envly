efbcd522faf7401577f98c392b428a7c35915a93
Christopher EnyTC <chris@enytc.com>
2015-12-22 08:37:38 -0200

    Add test stack for envly.

---
+++ spec/envly_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Envly do
+  it 'has a version number' do
+    expect(Envly::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+require 'envly'
+require 'faker'
